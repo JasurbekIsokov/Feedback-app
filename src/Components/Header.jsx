8e92d0eed78162b0ae614a6b156169de4fa47006
Jasurbek <jasurisoqov685@gmail.com>
2022-06-25 11:23:17 +0500

    Header title style

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,8 +1,13 @@
 // import PropTypes from "prop-types";
 
-function Header({ text }) {
+function Header({ text, bgColor, textColor }) {
+  const headerStyles = {
+    backgroundColor: bgColor,
+    color: textColor,
+  };
+
   return (
-    <header>
+    <header style={headerStyles}>
       <div className="container">
         <h1>{text}</h1>
       </div>
@@ -12,6 +17,8 @@ function Header({ text }) {
 
 Header.defaultProps = {
   text: "Feedback UI",
+  bgColor: "rgba(0,0,0,0.4",
+  textColor: "#ff6a95",
 };
 
 // Header.PropTypes = {
